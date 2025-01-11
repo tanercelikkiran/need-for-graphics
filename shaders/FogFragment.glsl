94f2a9c851332512319b28cf0ce4c6227f1f87c0
Barış Kılıç <bariskilic2003@gmail.com>
2025-01-12 01:53:11 +0300

    edited fog shader to match shadow shader

--- shaders/FogFragment.glsl
+++ shaders/FogFragment.glsl
@@ -4,7 +4,7 @@ in vec2 vUV;
 in float vFogDepth;
 out vec4 outColor;
 
-uniform sampler2D uDiffuseMap;
+uniform sampler2D diffuseMap;
 uniform float uFogNear;
 uniform float uFogFar;
 uniform vec3 uFogColor;
@@ -14,7 +14,7 @@ uniform vec3 uSolidColor;
 void main() {
     vec3 baseColor;
     if (uHasTexture) {
-        baseColor = texture(uDiffuseMap, vUV).rgb; // Use the texture color
+        baseColor = texture(diffuseMap, vUV).rgb; // Use the texture color
     } else {
         baseColor = uSolidColor; // Fallback to solid color
     }
